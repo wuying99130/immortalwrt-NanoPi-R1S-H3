6e788db780b0f7a28c1bf2120a5021f9f387af88
wuying99130 <wuying99130@gmail.com>
2026-08-25 12:06:06 +0800

    Update git clone URL for luci-app-sing-box plugin

--- build.sh
+++ build.sh
@@ -102,7 +102,7 @@ fi
 # 3. 精准下载 sing-box 面板插件
 if [ ! -d "$CUSTOM_PLUGIN_DIR/luci-app-sing-box" ]; then
     log_prog "-> 正在克隆 luci-app-sing-box 插件..."
-    git clone --depth=1 "https://${G_SITE}/${P_SINGBOX}" "$CUSTOM_PLUGIN_DIR/luci-app-sing-box"
+    git clone --depth=1 "https://github.com/SBWW/luci-app-sing-box.git" "$CUSTOM_PLUGIN_DIR/luci-app-sing-box"
 fi
 
 # 再次执行 defconfig，让系统正式把刚克隆进来的插件纳入编译索引
